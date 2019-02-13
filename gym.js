3c59cc0137a88ad74a2b08ca3ca2a91d46dd7c88
melody99621 <46924996+melody99621@users.noreply.github.com>
2019-02-13 20:26:58 +0800

    Add files via upload

--- gym.js
+++ gym.js
@@ -493,20 +493,20 @@ function initMap() {
         	});
 			//add marker
 			var infowindow = new google.maps.InfoWindow({});
-    		var	marker,i;
-    		
-				for (i = 0; i < locations.length; i++) {  
-    	  			marker = new google.maps.Marker({
-        			position: new google.maps.LatLng(locations[i][1], locations[i][2]),
-        			map: map1
-      				});
+    		var marker, i;
+
+				for (i = 0; i < locations.length; i++) {
+					marker = new google.maps.Marker({
+					position: new google.maps.LatLng(locations[i][1], locations[i][2]),
+					map: map1
+				});
     	  			
-    	  		google.maps.event.addListener(marker, 'click', (function(marker, i) {
-        			return function() {
-          			infowindow.setContent(locations[i][0]);
-          			infowindow.open(map, marker);
-        				}
-      				})(marker, i));
-    	  		}
+    	  		google.maps.event.addListener(marker, 'click', (function (marker, i) {
+					return function () {
+					infowindow.setContent(locations[i][0]);
+					infowindow.open(map, marker);
+					}
+					})(marker, i));
+				}
     	  		
 		}
